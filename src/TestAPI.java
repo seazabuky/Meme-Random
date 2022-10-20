3a83bdc0f113fe13dda938c86e5244330a34ef17
saltswater <seazabuky@gmail.com>
2022-10-20 21:05:19 +0700

    semi-final

--- src/TestAPI.java
+++ src/TestAPI.java
@@ -4,10 +4,11 @@
 import javax.swing.*;
 import java.net.*;
 import java.awt.*;
+import java.awt.event.*;
 
 
 
-public class TestAPI{
+public class TestAPI implements ActionListener{
    static UrlContent urlContent;
    static String API_KEY;
    static URL  url;
@@ -16,9 +17,10 @@ public class TestAPI{
    static JFrame frame;
    static DoubleLinkedList list = new DoubleLinkedList();
    static Desktop desk=Desktop.getDesktop();
+   static int click=0;
     public static void main(String[] args) throws Exception, MalformedURLException {
         try{
-         String input = "";
+        //  String input = "";
          API_KEY = "GYeHAfTxIoSwKJYD2tsY6XVfT1Q0yZfM";
         
         //Image zone
@@ -28,50 +30,86 @@ public class TestAPI{
         icon = new ImageIcon(url);
         label = new JLabel(icon);
         frame = new JFrame("Meme random");
-        frame.getContentPane().add(label);
+        // frame.getContentPane().add(label);
 
         //button zone
-        // JPanel panel = new JPanel();
-        // Button previous = new Button("Previous");
-        // Button changeTag = new Button("Change tag");
-        // Button next = new Button("Next");
-        // panel.add(previous);
-        // panel.add(changeTag);
-        // panel.add(next);
+        JPanel panel = new JPanel();
+        JButton previousBtn = new JButton("Previous");
+
+        JButton changeTagBtn = new JButton("Change tag");
+
+        JButton nextBtn = new JButton("Next");
+        nextBtn.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e){
+                try{
+                next();
+                }catch(Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+             }
+        });
+        previousBtn.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e){
+                try{
+                previous();
+                }catch(Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+             }
+        });
+        changeTagBtn.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e){
+                try{
+                    String tag = JOptionPane.showInputDialog("Input Tags");
+                    tag=tag.toLowerCase().replace(" ","-");
+                    urlContent = new UrlContent(API_KEY,tag);
+                    list.findLast();
+                    update();
+                }catch(Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+             }
+        });
+        panel.add(previousBtn);
+        panel.add(changeTagBtn);
+        panel.add(nextBtn);
 
         //Adding to Frame
-        // frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        // frame.getContentPane().add(BorderLayout.CENTER, label);
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.CENTER, label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1080,720);
+        frame.setSize(new Dimension(800, 600));
         frame.setResizable(false);
         frame.setVisible(true);
         
 
-        while(true){
-        input = JOptionPane.showInputDialog("Input");
-        if(input==null){
-            throw new Exception("exit");
-        }
-      if(input.equals("exit")){
-               System.exit(0);
-               break;
-        }else if(input.equals("n")){
-            next();
-        }
-        else if(input.equals("p")){
-            previous();
-        }
-        else if(input.equals("t")){
-               String tag = JOptionPane.showInputDialog("Input Tags");
-               tag=tag.toLowerCase().replace(" ","-");
-               urlContent = new UrlContent(API_KEY,tag);
-               list.findLast();
-               update();
-        }else if(input.equals("url")){
-            desk.browse(new URI((String)list.retrieve()));
-  }
-      }
+//         while(true){
+//         input = JOptionPane.showInputDialog("Input");
+//         if(input==null){
+//             throw new Exception("exit");
+//         }
+//       if(input.equals("exit")){
+//                System.exit(0);
+//                break;
+//         }else if(input.equals("n")){
+//             next();
+//         }
+//         else if(input.equals("p")){
+//             previous();
+//         }
+//         else if(input.equals("t")){
+//                String tag = JOptionPane.showInputDialog("Input Tags");
+//                tag=tag.toLowerCase().replace(" ","-");
+//                urlContent = new UrlContent(API_KEY,tag);
+//                list.findLast();
+//                update();
+//         }else if(input.equals("url")){
+//             desk.browse(new URI((String)list.retrieve()));
+//   }
+//       }
       }catch(Exception e){
           JOptionPane.showMessageDialog(null, e.getMessage());
           System.exit(0);
@@ -87,6 +125,7 @@ public class TestAPI{
       frame.setVisible(true);
    }
    public static void previous() throws MalformedURLException, Exception{
+      click--;
       list.findPrevious();
       String temp = (String)list.retrieve();
       url = new URL(temp);
@@ -96,6 +135,7 @@ public class TestAPI{
       frame.setVisible(true);
    }
    public static void next() throws MalformedURLException, Exception{
+      click++;
       if(list.getNext() != null){
       list.findNext();
       String temp = (String)list.retrieve();
@@ -106,8 +146,24 @@ public class TestAPI{
       frame.setVisible(true);
       }else
          update();
-      
    }
+   
+//    public static void checkClick(){
+//     if(click==0){
+//         previousBtn.setVisible(false);
+//         previousBtn.setEnabled(false);
+//     }else{
+//         previousBtn.setVisible(true);
+//         previousBtn.setEnabled(true);
+//     }
+//    }
+
+@Override
+public void actionPerformed(ActionEvent e) {
+    // TODO Auto-generated method stub
+    
+}
+
 
 }
 
